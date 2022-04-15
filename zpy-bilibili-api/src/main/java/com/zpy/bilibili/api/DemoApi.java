c09e0383facf44c9efa3724241bf629d175aebec
张鹏宇 <2247304904@qq.com>
2022-04-16 00:47:49 +0800

    2020-4-16 mybatis测试

---
+++ zpy-bilibili-api/src/main/java/com/zpy/bilibili/api/DemoApi.java
@@ -0,0 +1,25 @@
+package com.zpy.bilibili.api;
+
+import com.zpy.bilibili.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: zpy-bilibili
+ * @description: DemoApi
+ * @author: 张鹏宇
+ * @create: 2022-04-16 00:42
+ **/
+@RestController
+public class DemoApi {
+
+    @Autowired
+    private DemoService demoService;
+
+
+    @GetMapping("/query")
+    public Long query(Long id){
+        return demoService.query(id);
+    }
+}
---
+++ zpy-bilibili-service/src/main/java/com/zpy/bilibili/service/DemoService.java
@@ -0,0 +1,23 @@
+package com.zpy.bilibili.service;
+
+import com.zpy.bilibili.dao.DemoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: zpy-bilibili
+ * @description: DemoService
+ * @author: 张鹏宇
+ * @create: 2022-04-16 00:40
+ **/
+
+@Service
+public class DemoService {
+
+    @Autowired
+    private DemoDao demoDao;
+
+    public Long query(Long id) {
+        return demoDao.query(id);
+    }
+}
